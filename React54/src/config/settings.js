3348f5bfc5c9b7c72df9121f496da86e088ed870
lemaiquoccuong1611 <lemaiquoccuong@gmail.com>
2021-08-02 23:12:26 +0700

    first commit

---
+++ React54/src/config/settings.js
@@ -0,0 +1,7 @@
+export const settings = {
+    domain: 'http://movie0706.cybersoft.edu.vn/api',
+    domainLocal: 'http://localhost:5000/api',
+    userLogin: 'userLogin',
+    token: 'token',
+    groupID: 'GP07'
+}
